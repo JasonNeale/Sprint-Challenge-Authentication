42c214d4cc6d8b4d3f30019a9fb66002c7b694b2
Jason Neale <jg13.1980@gmail.com>
2020-06-15 05:55:04 -0400

    Updated

--- auth/auth-router.spec.js
+++ auth/auth-router.spec.js
@@ -1,13 +1,7 @@
-const request = require('request')
-const router = require('express').Router()
+const server = require('../api/server')
 const db = require('../data/dbConfig')
-const bcryptjs = require('bcryptjs')
-const {add} = require('../users/users-model')
+const request = require('supertest')
 
-
-beforeEach(async () => {
-  await db('users').truncate()
-})
 
 const user = {
   username: 'Test User',
@@ -16,29 +10,32 @@ const user = {
 
 describe('tests auth api functionality', () => {
   it('/api/auth/register endpoint - should return 201 (created)', () => {
-    return request(router)
+    const runTest = request(server)
     .post('/api/auth/register')
     .send(user)
     .expect(201)
   })
 
   it('/api/auth/register endpoint - should return 500 (server error)', () => {
-    return request(router)
+    const runTest = request(server)
     .post('/api/auth/register')
-    .send(user+user)
+    .send({
+      username: user.username,
+      username: user.username,
+      password: user.password
+    })
     .expect(500)
   })
 
   it('/api/auth/login endpoint - should return 401 (not found)', () => {
-    router.post('/register', async (req, res) => {
-    return request(router)
+    const runTest = request(server)
     .post('/api/auth/login')
     .send(user)
     .expect(401)
   })
 
   it('/api/auth/login endpoint - should return 500 (server error)', () => {
-    return request(router)
+    const runTest = request(server)
     .post('/api/auth/login')
     .send(user+user)
     .expect(500)
